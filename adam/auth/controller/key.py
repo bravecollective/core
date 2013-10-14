fa18ba438a2292a042ac286e4299c910c0fe95ed
Matthew Glinski <matthewglinski@gmail.com>
2013-10-14 17:15:17 -0400

    Update to fix my overzealous editor settings

--- adam/auth/controller/key.py
+++ adam/auth/controller/key.py
@@ -11,51 +11,51 @@ from adam.auth.util.predicate import authorize, authenticated, is_administrator
 
 
 class KeyInterface(HTTPMethod):
-	def __init__(self, key):
-		super(KeyInterface, self).__init__()
+    def __init__(self, key):
+        super(KeyInterface, self).__init__()
 
-		try:
-			self.key = EVECredential.objects.get(id=key)
-		except EVECredential.DoesNotExist:
-			raise HTTPNotFound()
+        try:
+            self.key = EVECredential.objects.get(id=key)
+        except EVECredential.DoesNotExist:
+            raise HTTPNotFound()
 
-		if self.key.owner.id != user.id:
-			raise HTTPUnauthorized()
+        if self.key.owner.id != user.id:
+            raise HTTPUnauthorized()
 
-	def delete(self):
-		self.key.delete()
+    def delete(self):
+        self.key.delete()
 
-		if request.is_xhr:
-			return 'json:', {'success': True}
+        if request.is_xhr:
+            return 'json:', {'success': True}
 
-		raise HTTPFound(location='/key/')
+        raise HTTPFound(location='/key/')
 
 
 class KeyList(HTTPMethod):
-	@authorize(authenticated)
-	def get(self, admin=False):
-		if admin and not user.admin:
-			raise HTTPUnauthorized("Must be administrative user.")
+    @authorize(authenticated)
+    def get(self, admin=False):
+        if admin and not user.admin:
+            raise HTTPUnauthorized("Must be administrative user.")
 
-		return "adam.auth.template.key.list", dict(area='keys', admin=bool(admin), records=user.credentials)
+        return "adam.auth.template.key.list", dict(area='keys', admin=bool(admin), records=user.credentials)
 
-	@authorize(authenticated)
-	def post(self, **kw):
-		data = Bunch(kw)
+    @authorize(authenticated)
+    def post(self, **kw):
+        data = Bunch(kw)
 
-		record = EVECredential(data.key, data.code, owner=user.id)
-		record.save()
+        record = EVECredential(data.key, data.code, owner=user.id)
+        record.save()
 
-		if request.is_xhr:
-			return 'json:', {'success': True, 'identifier': str(record.id), 'key': record.key, 'code': record.code}
+        if request.is_xhr:
+            return 'json:', {'success': True, 'identifier': str(record.id), 'key': record.key, 'code': record.code}
 
-		raise HTTPFound(location='/key/')
+        raise HTTPFound(location='/key/')
 
 
 class KeyController(Controller):
-	"""Entry point for the KEY management RESTful interface."""
+    """Entry point for the KEY management RESTful interface."""
 
-	index = KeyList()
+    index = KeyList()
 
-	def __lookup__(self, key, *args, **kw):
-		return KeyInterface(key), args
+    def __lookup__(self, key, *args, **kw):
+        return KeyInterface(key), args
