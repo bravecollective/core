adda8fd9bb3f90f98be183c33f15bd772b68f38c
Alice Bevan-McGregor <alice@gothcandy.com>
2013-11-26 10:55:36 -0500

    Split out the “dev tools”.

--- brave/core/controller.py
+++ brave/core/controller.py
@@ -2,13 +2,51 @@
 
 from __future__ import unicode_literals
 
-from web.core import Controller
+from web.core import Controller, config
 from web.core.locale import set_lang, LanguageError
+from marrow.util.convert import boolean
 
 from brave.core import util
 from brave.core.util.signal import StartupMixIn
 from brave.core.util.predicate import authorize, authenticated
+from brave.core.api.client import API
 
+
+
+class DeveloperTools(Controller):
+    def die(self):
+        """Simply explode.  Useful to get the interactive debugger up."""
+        1/0
+    
+    def test(self):
+        """Return an HTML/templating scratch pad."""
+        return 'brave.core.template.test', dict(data=None)
+    
+    def authn(self):
+        """Request the API to authenticate a user, see what we get back."""
+        
+        # To make API calls, we need the API keys.
+        # The private key is *our* private key, to sign requests.
+        # The public key is the *server's* public key, given after registering your application.
+        # We're cheating here so they're one and the same for simplicity.
+        # (Generally you'd do this once on startup.)
+        from binascii import hexlify, unhexlify
+        from hashlib import sha256
+        from ecdsa.keys import SigningKey, VerifyingKey
+        from ecdsa.curves import NIST256p
+        private = SigningKey.from_string(unhexlify(config['api.key']), curve=NIST256p, hashfunc=sha256)
+        public = private.get_verifying_key()
+        
+        # Construct an API instance to get easy attribute-access to the remote functions.
+        api = API(config['api.endpoint'], config['api.identity'], private, public)
+        
+        # We request an authenticated session from the server.
+        result = api.authorize(
+                success = 'http://localhost:8080/dev/win',
+                failure = 'http://localhost:8080/dev/fail'
+            )
+        
+        return 'brave.core.template.test', dict(data=result)
 
 
 class RootController(StartupMixIn, Controller):
@@ -17,6 +55,12 @@ class RootController(StartupMixIn, Controller):
     character = util.load('character')
     application = util.load('application')
     api = util.load('api')
+    
+    def __init__(self, *args, **kw):
+        super(RootController, self).__init__(*args, **kw)
+        
+        if boolean(config.get('debug', False)):
+            self.dev = DeveloperTools()
     
     @authorize(authenticated)
     def index(self):
@@ -29,3 +73,4 @@ class RootController(StartupMixIn, Controller):
             return 'json:', dict(success=False)
         
         return 'json:', dict(success=True)
+    
