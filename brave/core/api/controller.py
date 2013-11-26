e4fc553f614735017aea4b876f65661005343d10
Alice Bevan-McGregor <alice@gothcandy.com>
2013-11-26 10:55:21 -0500

    Added mock authorization endpoint.

--- brave/core/api/controller.py
+++ brave/core/api/controller.py
@@ -2,12 +2,11 @@
 
 from __future__ import unicode_literals
 
-from web.core import request
+from web.core import request, url
 from brave.core.api.util import SignedController
 
 
 log = __import__('logging').getLogger(__name__)
-
 
 
 class ApiController(SignedController):
@@ -21,3 +20,13 @@ class ApiController(SignedController):
                 request.service.id, (our - int(now)) if now else "unknown")
         
         return dict(now=our)
+    
+    def authorize(self, success, failure):
+        """Prepare a incoming session request."""
+        
+        return dict(
+                youare = str(request.service.id),
+                location = url.complete('/authorize/someidhere'),
+                success = success,
+                failure = failure
+            )
