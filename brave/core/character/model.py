2a081b8d0491c1c139d8eca86d56f7d3083f7c3a
Alice Bevan-McGregor <alice@gothcandy.com>
2014-02-02 12:53:37 -0500

    Calculating all Group membership for a character.

--- brave/core/character/model.py
+++ brave/core/character/model.py
@@ -2,6 +2,7 @@
 
 from __future__ import unicode_literals
 
+from collections import OrderedDict
 from datetime import datetime
 from mongoengine import Document, StringField, DateTimeField, ReferenceField, IntField, BooleanField
 from brave.core.util.signal import update_modified_timestamp
@@ -52,3 +53,17 @@ class EVECharacter(EVEEntity):
     
     owner = ReferenceField('User', db_field='o', reverse_delete_rule='NULLIFY')
     credential = ReferenceField(EVECredential, db_field='r', reverse_delete_rule='NULLIFY')
+    
+    @property
+    def tags(self):
+        from brave.core.group.model import Group
+        mapping = dict()
+        
+        for group in Group.objects:
+            if group.evaluate(self.owner, self):
+                mapping[group.id] = group
+        
+        def titlesort(i):
+            return mapping[i].title
+        
+        return OrderedDict((i, mapping[i]) for i in sorted(mapping.keys(), key=titlesort))
