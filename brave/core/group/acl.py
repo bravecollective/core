67bdf0a78c3b9498c2000ddb122f1ed1d51162b2
Alice Bevan-McGregor <alice@gothcandy.com>
2014-02-04 16:11:22 -0500

    New ACL rules to utilize new data.

--- brave/core/group/acl.py
+++ brave/core/group/acl.py
@@ -21,12 +21,15 @@ class ACLRule(EmbeddedDocument):
     # ACL rules evaluate to None (doesn't apply), False (deny access), and True (allow access)
     # the fist ACLRule for a group that evaluates non-None is the accepted result
     grant = BooleanField(db_field='g', default=False)  # paranoid default
+    inverse = BooleanField(db_field='z', default=False)  # pass/fail if the rule *doesn't* match
     
     def evaluate(self, user, character):
         raise NotImplementedError()
 
 
 class ACLList(ACLRule):
+    """Grant or deny access based on the character's ID, corporation ID, or alliance ID."""
+    
     KINDS = OrderedDict([
             ('c', "Character"),
             ('o', "Corporation"),
@@ -47,6 +50,52 @@ class ACLList(ACLRule):
     
     def evaluate(self, user, character):
         if getattr(self, 'evaluate_' + self.KINDS[self.kind].lower())(user, character):
-            return self.grant
+            return None if self.inverse else self.grant
         
-        return None  # this acl rule doesn't match or is not applicable
+        # this acl rule doesn't match or is not applicable
+        return self.grant if self.inverse else None
+
+
+class ACLKey(ACLRule):
+    """Grant or deny access based on the character's key type."""
+    
+    KINDS = OrderedDict([
+            ('Account', "Account"),
+            ('Character', "Character"),
+            ('Corporation', "Corporation")
+        ])
+    
+    kind = StringField(db_field='k', choices=KINDS.items())
+    
+    def evaluate(self, user, character):
+        for key in character.credentials:
+            if key.kind == self.kind:
+                return None if self.inverse else self.grant
+        
+        return self.grant if self.inverse else None
+
+
+class ACLTitle(ACLRule):
+    """Grant or deny access based on the character's corporate title."""
+    
+    titles = ListField(StringField(), db_field='t')
+    
+    def evaluate(self, user, character):
+        if set(character.titles) & set(self.titles):
+            return None if self.inverse else self.grant
+        
+        # this acl rule doesn't match or is not applicable
+        return self.grant if self.inverse else None
+
+
+class ACLRole(ACLRule):
+    """Grant or deny access based on the character's corporate role."""
+    
+    roles = ListField(StringField(), db_field='t')
+    
+    def evaluate(self, user, character):
+        if set(character.roles) & set(self.roles):
+            return None if self.inverse else self.grant
+        
+        # this acl rule doesn't match or is not applicable
+        return self.grant if self.inverse else None
