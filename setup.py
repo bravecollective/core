0a8a3605d94fe058d8bd36c551e8fbf07f43a8c1
Alice Bevan-McGregor <alice@gothcandy.com>
2014-02-20 02:10:27 -0500

    Replaced XML parser dep.

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
                 'blinker',
                 'pyyaml',
                 'ecdsa',
-                'xmltodict',
+                'relaxml',
                 'ipython',
                 'scrypt',
                 'pudb',
