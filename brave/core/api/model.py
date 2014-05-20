11af7fa28ce1eb55ad7156a9495c9788852a138c
Tyler O'Meara <Tyler@TylerOMeara.com>
2014-05-20 00:24:40 -0500

    Create Ban, CharacterBan, and IPBan objects, and create a new DB schema for bans against users.
    Signed-off-by: Tyler O'Meara <Tyler@TylerOMeara.com>

--- brave/core/api/model.py
+++ brave/core/api/model.py
@@ -8,12 +8,95 @@ from mongoengine import Document, EmbeddedDocument, EmbeddedDocumentField, Strin
 from brave.core.util.signal import update_modified_timestamp
 from brave.core.application.signal import trigger_private_key_generation
 from brave.core.util.field import PasswordField, IPAddressField
+from brave.core.account.model import User
 
 
 log = __import__('logging').getLogger(__name__)
 
-
+    
+class CharacterBan(EmbeddedDocument):
+    """Stores information about character bans."""
+    
+    meta = dict(
+            allow_inheritance = False,
+            indexes = [
+                    dict(fields=['name'], unique=True),
+                    dict(fields=['enabled'])
+                ]
+        )
+        
+    # The character this ban is for.
+    # Uses a string rather than a reference so users can't get around
+    # the blacklist by deleting the banned character or account
+    character = StringField(db_field='name')
+    
+    # The date at which this character was banned.
+    date = DateTimeField(db_field='created', default=datetime.utcnow)
+    
+    # The reason indicates how this character came to be banned.
+    # 'direct' means the (overall) ban was invoked against this character in particular.
+    # 'account' means the (overall) ban was invoked against the account that owned this character.
+    # 'key' means this character was found on the same key as another banned character.
+    reason = StringField(db_field='reason', choices=((
+            ('direct'),
+            ('account'),
+            ('key')
+        )))
+    
+    enabled = BooleanField(db_field='enabled', default=True)
+    
+    
+class IPBan(EmbeddedDocument):
+    """Stores information about bans against an IP Address."""
+    
+    meta = dict(
+            allow_inheritance = False,
+            indexes = [
+                    'host',
+                    'enabled'
+                ]
+        )
+    
+    host = StringField('IP')
+    
+    # The date at which this character was banned.
+    date = DateTimeField(db_field='created', default=datetime.utcnow)
+    
+    # The reason indicates how this character came to be banned.
+    # 'direct' means the (overall) ban was invoked against this IP Address in particular.
+    # 'account' means the (overall) ban was invoked against the account that this IP was registered against.
+    reason = StringField(db_field='reason', choices=((
+            ('direct'),
+            ('account')
+        )))
+        
+    enabled = BooleanField(db_field='enabled', default=True)
+    
+    
+class Ban(Document):
+    """Holds information about a single ban.
+    
+    Multiple characters and/or IP addresses can be encompassed in
+    one ban object."""
+    
+    meta = dict(
+            allow_inheritance = False,
+            indexes = [
+                    dict(fields=['creator', 'enabled', 'charCreator'])
+                ]
+        )
+    
+    characters = ListField(EmbeddedDocumentField(CharacterBan))
+    IPs = ListField(EmbeddedDocumentField(IPBan))
+    
+    enabled = BooleanField(db_field='enabled', default=True)
+    creator = ReferenceField(User, required=True) # TODO: Nullify inverse deletion rule.
+    charCreator = StringField(db_field='charCreator', required=True)
+    
+    
 class AuthenticationBlacklist(Document):
+    """Blacklist for applications and servers."""
+    
     meta = dict(
             allow_inheritance = False,
             indexes = [
@@ -30,7 +113,7 @@ class AuthenticationBlacklist(Document):
     port = StringField('o')
     
     creator = ReferenceField('User')  # TODO: Nullify inverse deletion rule.
-
+        
 
 class AuthenticationRequest(Document):
     meta = dict(
