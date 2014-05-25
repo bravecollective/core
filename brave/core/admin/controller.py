e045f3a072449351b9b234544c69a8f153ab14b2
Tyler O'Meara <Tyler@TylerOMeara.com>
2014-05-24 23:39:37 -0500

    Change the search functions to be on different pages, and make the arguments lowercased.
    Signed-off-by: Tyler O'Meara <Tyler@TylerOMeara.com>

--- brave/core/admin/controller.py
+++ brave/core/admin/controller.py
@@ -16,56 +16,44 @@ from brave.core.util.predicate import authorize, is_administrator
 from brave.core.key.model import EVECredential
 from brave.core.account.model import User
 
-class AdminInterface(HTTPMethod):
+
+class SearchCharInterface(HTTPMethod):
+    """Handles /admin/search/char"""
     
-    @authorize(is_administrator)
     def get(self):
-        return 'brave.core.admin.template.search', dict(
-            )
-
-class AdminController(Controller):
-    """Entry point for the Search RESTful interface."""
-
-    #index = AdminInterface()
-
-    def index(self):
-        return 'brave.core.admin.template.search', dict(area='admin')
-    
-    def search(self, Character=None, charMethod=None, Alliance=None, Corporation=None, group=None, KeyID=None,
-                KeyMask=None, Username=None, userMethod=None, IP=None):
-        """Handles the /admin/search page, which is the post result of /admin/."""
+        return 'brave.core.admin.template.searchChar', dict(area='admin')
+        
+    def post(self, character=None, charMethod=None, alliance=None, corporation=None, group=None):
         
         # Have to be an admin to access admin pages.            
         if not is_administrator:
             raise HTTPNotFound()
         
-        # Seed the initial results with all of the respective objects.
+        # Seed the initial results.
         chars = EVECharacter.objects()
-        keys = EVECredential.objects()
-        users = User.objects()
             
         # Go through and check all of the possible posted values
         
         # Limit chars to the character name entered.
-        if Character:
+        if character:
             if charMethod == 'contains':
-                chars = chars.filter(name__icontains=Character)
+                chars = chars.filter(name__icontains=character)
             elif charMethod == 'starts':
-                chars = chars.filter(name__istartswith=Character)
+                chars = chars.filter(name__istartswith=character)
             elif charMethod == 'is':
-                chars = chars.filter(name__iexact=Character)
+                chars = chars.filter(name__iexact=character)
             else:
                 return 'json:', dict(success=False, message=_("You broke the web page. Good Job."))
         
         # Limit to characters in the specified alliance.
-        if Alliance:
-            Alliance = EVEAlliance.objects(name=Alliance).first()
-            chars = chars.filter(alliance=Alliance)
+        if alliance:
+            alliance = EVEAlliance.objects(name=alliance).first()
+            chars = chars.filter(alliance=alliance)
         
         # Limit to characters in the specified corporation.
-        if Corporation:
-            Corporation = EVECorporation.objects(name=Corporation).first()
-            chars = chars.filter(corporation=Corporation)
+        if corporation:
+            corporation = EVECorporation.objects(name=corporation).first()
+            chars = chars.filter(corporation=corporation)
         
         # Limit to characters in the specified group.
         if group:
@@ -75,49 +63,83 @@ class AdminController(Controller):
                     groupList.append(c.id)
                     
             chars = chars.filter(id__in=groupList)
+            
+        return 'brave.core.admin.template.searchChar', dict(area='admin', result=chars, success=True)
+
+
+class SearchKeyInterface(HTTPMethod):
+    """Handles /admin/search/key"""
+    def get(self):
+        return 'brave.core.admin.template.searchKey', dict(area='admin')
+        
+    def post(self, keyID=None, keyMask=None, violation=None):
+        # Seed the initial results.
+        keys = EVECredential.objects()
         
         # Limit to keys with the specified ID.
-        if KeyID:
-            keys = keys.filter(key=KeyID)
+        if keyID:
+            keys = keys.filter(key=keyID)
         
         # Limit to keys with the specified Mask.
-        if KeyMask:
-            keys = keys.filter(_mask=KeyMask)
+        if keyMask:
+            keys = keys.filter(_mask=keyMask)
+            
+        # Limit to keys with the specified violation.
+        if violation.lower() == "none":
+            keys = keys.filter(violation=None)
+        elif violation:
+            keys = keys.filter(violation__iexact=violation)
+            
+        return 'brave.core.admin.template.searchKey', dict(area='admin', result=keys, success=True)
+        
+        
+class SearchUserInterface(HTTPMethod):
+    """Handles /admin/search/user"""
+    def get(self):
+        return 'brave.core.admin.template.searchUser', dict(area='admin')
+        
+    def post(self, username=None, userMethod=None, ip=None, duplicate=None):
+        
+        # Seed the initial results.
+        users = User.objects()
         
         # Limit to users with the specified username.
-        if Username:
+        if username:
             if userMethod == 'contains':
-                users = users.filter(username__icontains=Username)
+                users = users.filter(username__icontains=username)
             elif userMethod == 'starts':
-                users = users.filter(username__istartswith=Username)
+                users = users.filter(username__istartswith=username)
             elif userMethod == 'is':
-                users = users.filter(username__iexact=Username)
+                users = users.filter(username__iexact=username)
             else:
                 return 'json:', dict(success=False, message=_("You broke the web page. Good Job."))
         
         # Limit to users with the specified IP address.
-        if IP:
-            users = users.filter(host=IP)
+        if ip:
+            users = users.filter(host=ip)
+            
+        # Limit to users with the specified duplicate status
+        if duplicate.lower() == "ip":
+            users = users.filter(other_accs_IP__exists=True)
+        elif duplicate.lower() == "char":
+            users = users.filter(other_accs_char_key__exists=True)
+            
+        return 'brave.core.admin.template.searchUser', dict(area='admin', result=users, success=True)
+        
+        
+class SearchController(Controller):
+    char = SearchCharInterface()
+    key = SearchKeyInterface()
+    user = SearchUserInterface()
+    
+    
+class AdminController(Controller):
+    """Entry point for the Search RESTful interface."""
 
-        # Only one search row is returned.
-        # Choose to return characters > keys > users.
-        if Character or Alliance or Corporation or group:
-            kind = 'Character'
-            results = chars
-        elif KeyID or KeyMask or KeyMask == 0:
-            kind = 'Key'
-            results = keys
-        elif Username or IP:
-            kind = 'User'
-            results = users
-        else:
-            return 'json:', dict(success=False, message=_("You broke the web page. Good Job."))
+    search = SearchController()
 
-        result = []
-        for obj in results:
-            result.append(obj)
-
-        return 'brave.core.admin.template.search', dict(success=True, kind=kind, result=result)
+    def index(self):
+        return 'brave.core.admin.template.search', dict(area='admin')
     
     def __lookup__(self, key, *args, **kw):
         request.path_info_pop()  # We consume a single path element.
