409756df92c63e7eadd2eac28798bf2c1b45f133
Tyler O'Meara <Tyler@TylerOMeara.com>
2014-09-01 21:38:11 -0500

    Move the runtime permission handling code into it's own decorator.
    Signed-off-by: Tyler O'Meara <Tyler@TylerOMeara.com>

--- brave/core/permission/util.py
+++ brave/core/permission/util.py
@@ -11,41 +11,18 @@ import web.auth
 log = __import__('logging').getLogger(__name__)
 
 
-def user_has_permission(perm=None, **runkw):
-    """This decorator checks if a user has the permission 'perm'. This is intended as a convenient way to check for
-    a user's ability to conduct an action at the beginning of functions, such as for HTTP GET and POST requests. If a
-    user does not have the permission sepcified, the decorator raises a 403 error. **runkw is used to allow for
-    checking permissions which are not known at compile time, such as when they rely on arguments supplied to the
-    decorated function. To use **runkw, set up perm as if you were formatting it using kw arguments, and then supply
-    those kw arguments to the decorator as a string."""
-        
+def prepare_runtime_permission(perm=None, **runkw):
+    """This decorator handles the runtime permission aspects of the user permission checking decorators."""
+    
     def decorator(function):
         
-        @authenticate
-        def check_permission(self, *args, **kwargs):
-            user = web.auth.user
+        def handle_runkw(self, *args, **kwargs):
+            permission = perm
             
-            # If there is no permission provided, then any auth'd user has permission
-            if not perm:
+            if not permission:
                 log.debug('No permission provided.')
-                return function(self, *args, **kwargs)
-            else:
-                # Can't modify perm due to scoping, so we duplicate it's value into a 'local' variable and use that.
-                permission = perm
-        
-            user = user._current_obj()
-        
-            # No user with that username was found.
-            if not user:
-                log.debug('User not found.')
-                raise HTTPForbidden()
+                return function(self, permission, *args, **kwargs)
             
-            # User has no characters, so they have no permissions.
-            if not len(user.characters):
-                log.debug('User has no characters.')
-                raise HTTPForbidden()
-                
-            # Handle run-time permissions.
             for key, value in runkw.iteritems():
                 valSplit = value.split('.')
                 for attr in valSplit:
@@ -58,6 +35,39 @@ def user_has_permission(perm=None, **runkw):
                     value = getattr(value, attr)
                     
                 permission = permission.replace('{'+key+'}', value)
+            
+            return function(self, permission, *args, **kwargs)
+            
+        return handle_runkw
+    
+    return decorator
+
+def user_has_permission(perm=None, **runkw):
+    """This decorator checks if a user has the permission 'perm'. This is intended as a convenient way to check for
+    a user's ability to conduct an action at the beginning of functions, such as for HTTP GET and POST requests. If a
+    user does not have the permission sepcified, the decorator raises a 403 error. **runkw is used to allow for
+    checking permissions which are not known at compile time, such as when they rely on arguments supplied to the
+    decorated function. To use **runkw, set up perm as if you were formatting it using kw arguments, and then supply
+    those kw arguments to the decorator as a string."""
+        
+    def decorator(function):
+        
+        @authenticate
+        @prepare_runtime_permission(perm, **runkw)
+        def check_permission(self, permission, *args, **kwargs):
+            user = web.auth.user
+        
+            user = user._current_obj()
+        
+            # No user with that username was found.
+            if not user:
+                log.debug('User not found.')
+                raise HTTPForbidden()
+            
+            # User has no characters, so they have no permissions.
+            if not len(user.characters):
+                log.debug('User has no characters.')
+                raise HTTPForbidden()
             
             # Cycle through the user's permissions, and if they have it leave the method.
             for c in user.characters:
@@ -80,16 +90,9 @@ def user_has_any_permission(perm=None, **runkw):
     def decorator(function):
         
         @authenticate
-        def check_permission(self, *args, **kwargs):
+        @prepare_runtime_permission(perm, **runkw)
+        def check_permission(self, permission, *args, **kwargs):
             user = web.auth.user
-            
-            # If there is no permission provided, then any auth'd user has permission
-            if not perm:
-                log.debug('No permission provided.')
-                return function(self, *args, **kwargs)
-            else:
-                # Can't modify perm due to scoping, so we duplicate it's value into a 'local' variable and use that.
-                permission = perm
         
             user = user._current_obj()
         
@@ -102,20 +105,6 @@ def user_has_any_permission(perm=None, **runkw):
             if not len(user.characters):
                 log.debug('User has no characters.')
                 raise HTTPForbidden()
-                
-            # Handle run-time permissions.
-            for key, value in runkw.iteritems():
-                valSplit = value.split('.')
-                for attr in valSplit:
-                    if attr == 'self':
-                        value = self
-                        continue
-                    elif attr in kwargs:
-                        value = kwargs.get(attr)
-                        continue
-                    value = getattr(value, attr)
-                    
-                permission = permission.replace('{'+key+'}', value)
 
             p = WildcardPermission.objects(id=permission).first()
             if not p:
