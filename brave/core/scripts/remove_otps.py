94dcfad384d9bb057c61f0ee9a27adf45ae4d28e
Tyler O'Meara <Tyler@TylerOMeara.com>
2014-10-03 19:18:18 -0500

    Fix the remove_otps script to work after the User changes have been applied.
    Signed-off-by: Tyler O'Meara <Tyler@TylerOMeara.com>

--- brave/core/scripts/remove_otps.py
+++ brave/core/scripts/remove_otps.py
@@ -1,4 +1,14 @@
-from brave.core.account.model import User
+from mongoengine import Document, ListField, StringField
+
+class User(Document):
+    meta = dict(
+        collection = 'Users',
+        allow_inheritance = False,
+        indexes = ['otp'],
+    )
+
+    username = StringField(db_field='u', required=True, unique=True, regex=r'[a-z][a-z0-9_.-]+')
+    otp = ListField(StringField(), default=list)
 
 def remove_otps(dry_run=True):
     """Remove OTPs from all users. Mainly intended to serve as a 'migration' from the old single
@@ -8,4 +18,5 @@ def remove_otps(dry_run=True):
             print u.username
             if not dry_run:
                 u.otp = None
+                u.rotp = None
                 u.save()
